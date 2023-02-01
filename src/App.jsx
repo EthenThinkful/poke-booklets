3ebdadb9b6010d09e2f22bca84c92679749d5b99
Ethen Roth <Ethenfm@gmail.com>
2023-01-31 20:20:10 -0700

    pushing for laptop work

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,21 @@ import './App.css'
 
 function App() {
   const [pokemon, setPokemon] = useState('');
-
+  function getPokemon() {
+    Axios.get
+  }
   return (
     <div className="App">
       <h1 className="text-3xl font-bold">Pokedex!</h1>
       <input
+      className='p-2 rounded-md'
       placeholder='search pokemon'
       type='text'
       onChange={(event) => {
         setPokemon(event.target.value);
       }}
       />
-      <button>search</button>
+      <button className='m-8 bg-orange-300 p-2 rounded-md'>search</button>
     </div>
   )
 }
