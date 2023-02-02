b9d192b1c5f3a54bb0cdb15749cd46a630695d3d
Ethen Roth <Ethenfm@gmail.com>
2023-02-01 17:43:08 -0700

    added search feature with pokemon image display

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,44 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Axios from "axios";
 
 function App() {
-  const [pokemon, setPokemon] = useState('');
+  const [pokemon, setPokemon] = useState("");
+  const [pokemonData, setPokemonData] = useState({});
+
   function getPokemon() {
-    Axios.get
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`).then(
+      (response) => {
+        setPokemonData({
+          name: pokemon,
+          species: response.data.species.name,
+          img: response.data.sprites.front_default,
+          hp: response.data.stats[0].base_stat,
+        });
+      }
+    );
   }
   return (
     <div className="App">
       <h1 className="text-3xl font-bold">Pokedex!</h1>
       <input
-      className='p-2 rounded-md'
-      placeholder='search pokemon'
-      type='text'
-      onChange={(event) => {
-        setPokemon(event.target.value);
-      }}
+        className="p-2 rounded-md w-48"
+        placeholder="search pokemon"
+        type="text"
+        onChange={(event) => {
+          setPokemon(event.target.value);
+        }}
       />
-      <button className='m-8 bg-orange-300 p-2 rounded-md'>search</button>
+      <button className="m-8 bg-orange-300 p-2 rounded-md" onClick={getPokemon}>
+        search
+      </button>
+      <div className="flex justify-center">
+      <img src={pokemonData.img}
+      className="m-16 w-52"
+      />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
