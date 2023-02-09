4eb9cc69b106f6e8f4e6bcad767e5d95921155d5
Ethen Roth <Ethenfm@gmail.com>
2023-02-09 14:41:28 -0700

    separated components and created start of component to console log api requests for pokemon abilities

--- src/App.jsx
+++ src/App.jsx
@@ -6,102 +6,16 @@ import Draggable from "react-draggable";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import pokeNames from "./assets/PokeJSON/pokeNames.json";
 import PopUp from "./PopUp";
+import PokemonRender from "./PokemonRender";
 
 function App() {
-  const [pokemon, setPokemon] = useState("");
-  const [pokemonData, setPokemonData] = useState([]);
 
-  function getPokemon() {
-    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`).then(
-      (response) => {
-        setPokemonData([
-          ...pokemonData,
-          {
-            name: pokemon,
-            species: response.data.species.name,
-            img: response.data.sprites.front_default,
-            hp: response.data.stats[0].base_stat,
-            key: pokemon,
-          },
-        ]);
-      }
-    );
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    getPokemon();
-  };
-
-  const handleKeypress = (e) => {
-    // triggers by pressing the enter key
-    if (e.keyCode === 13) {
-      handleSubmit();
-    }
-  };
-
-  const nodeRef = useRef(null); // do get around strictMode
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold sm: pb-6">Pokedex!</h1>
-      <form>
-        <input
-          className="p-2 rounded-md w-48 bg-zinc-800"
-          placeholder="search pokemon"
-          type="text"
-          value={pokemon}
-          onKeyDown={handleKeypress}
-          onChange={(event) => {
-            setPokemon(event.target.value.toLocaleLowerCase());
-            {
-              console.log(pokemon);
-            }
-          }}
-        />
-        <button
-          className="m-8 bg-orange-300 p-2 rounded-md"
-          type="submit"
-          onClick={handleSubmit}
-        >
-          Add Pokemon!
-        </button>
-        <div className="dropDown">
-          {pokeNames
-            .filter((item) => {
-              const searchTerm = pokemon.toLocaleLowerCase();
-              const fullName = item.toLocaleLowerCase();
-              return (
-                searchTerm &&
-                fullName.startsWith(searchTerm) &&
-                fullName !== searchTerm
-              );
-            })
-            .slice(0, 10)
-            .map((item) => (
-              <div
-                onClick={() => setPokemon(item.toLocaleLowerCase())}
-                key={item}
-                className="cursor-pointer"
-              >
-                {item}
-              </div>
-            ))}
-        </div>
-      </form>
-      <div className="flex justify-center">
-        {pokemonData.map((poke, i) => (
-          <Draggable key={i} nodeRef={nodeRef} >
-            <span ref={nodeRef} className="cursor-pointer" >
-              <TransformComponent >
-                <img src={poke.img} className="w-40 m-0 "/>
-                <PopUp />
-              </TransformComponent>
-            </span>
-          </Draggable>
-        ))}
-      </div>
+      <PokemonRender />
+      
     </div>
-  );
+  )
 }
 
 export default App;
--- src/PopUp.jsx
+++ src/PopUp.jsx
@@ -1,14 +1,42 @@
-import { useState } from "react"
+import React from "react";
+import { useState, useRef } from "react"
+import PokemonRender from "./PokemonRender"
+import Draggable from "react-draggable";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import Axios from "axios";
 
-export default function PopUp() {
-    const [click, setClick] = useState(false)
 
+export default function PopUp({pokemon}) {
+    
+    const [details, setDetails] = useState([])
 
-    // function handlePopUP() {
-    //     setClick(true)
-    // }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        getDetails();
+      };
+
+// https://pokeapi.co/api/v2/ability/{id or name}/
+    function getDetails() {
+        Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`).then(
+          (response) => {
+            setDetails([
+              ...details,
+              {
+                ability: response.data.abilities[0].ability.name
+                
+              },
+            ]);
+            console.log(response)
+          }
+          
+        );
+      }
+
+    const nodeRef = useRef(null);
 
     return (
-        <div >hello dudeski</div>
+        <Draggable nodeRef={nodeRef}  onClick={console.log("pokemon")}>
+            <button className="bg: bg-red-200 p-8 rounded-lg" ref={nodeRef}> hello dudeski </button>
+        </Draggable>
     )
 }
