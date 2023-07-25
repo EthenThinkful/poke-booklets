59d328c03014656952320c01501e694e141a0a16
Ethen Roth <Ethenfm@gmail.com>
2023-07-24 23:04:20 -0600

    re push (same as last commit)

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,12 @@ import CardRender from "./components/CardRender";
 import { DndProvider } from "react-dnd";
 import { MultiBackend } from 'react-dnd-multi-backend'
 import { HTML5toTouch } from 'rdndmb-html5-to-touch' // or any other pipeline
-import Test from "./components/test";
 
 function App() {
   return (
       <DndProvider backend={MultiBackend} options={HTML5toTouch}>
       <div className="font-pokeFont">
         <CardRender />
-        {/* <Test /> */}
       </div>
       </DndProvider>
     
