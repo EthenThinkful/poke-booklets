1a0d9608c53d2d893f8efd73869f7fe5f3b51841
Ethen Roth <Ethenfm@gmail.com>
2023-08-14 23:41:34 -0600

    made slider responsive

--- src/components/RenderCarousel.jsx
+++ src/components/RenderCarousel.jsx
@@ -1,56 +1,57 @@
 import { Carousel } from "react-responsive-carousel";
+import React, { useState, useEffect } from "react";
 
 export default function RenderCarousel({ carouselImg, setBook }) {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  function handleAddBookClick(src, id) {
+    setBook((book) => [...book, { src, id }]);
+  }
+
   let temp = [];
-  for (let i = 0; i < carouselImg.length; i += 2) {
-    let twoPack = carouselImg.slice(i, i + 2);
-    if (twoPack.length > 1) {
-      // console.log(twoPack[0].id)
-      // console.log(twoPack[1].id)
+  let chunkSize = windowWidth < 992 ? 2 : 6;
+
+  for (let i = 0; i < carouselImg.length; i += chunkSize) {
+    let chunk = carouselImg.slice(i, i + chunkSize);
+
     temp.push(
-      <div className="flex mt-2 mb-2">
-        <div>
-          <button
-            className="btn w-16 h-8 bg-slate-600 rounded-xl text-center"
-            onClick={() =>
-              setBook((book) => [...book, {src: twoPack[0].src, id: twoPack[0].id}])
-            }
-          >
-            +
-          </button>
-          <img className="carousel__card" src={twoPack[0].src} />
-        </div>
-        <div>
-          <button
-            className="btn w-16 h-8 bg-slate-600 rounded-xl text-center"
-            onClick={() =>
-              setBook((book) => [...book, {src: twoPack[1].src, id: twoPack[1].id}])
-            }
-          >
-            +
-          </button>
-          <img className="carousel__card" src={twoPack[1].src} />
-        </div>
+      <div className="flex mt-2 mb-2" key={i}>
+        {chunk.map((item) => (
+          <div key={item.id}>
+            <button
+              className="btn w-16 h-8 bg-slate-600 rounded-xl text-center"
+              onClick={() => handleAddBookClick(item.src, item.id)}
+            >
+              +
+            </button>
+            <img className="carousel__card" src={item.src} alt={`Book ${item.id}`} />
+          </div>
+        ))}
       </div>
     );
   }
-}
 
   function renderImgs(val) {
     // console.log(val);
-    return temp.map((item) => {
+    return val.map((item) => {
       return item;
     });
   }
 
   return (
     <>
-      <Carousel
-        autoFocus={true}
-        showThumbs={false}
-        showStatus={false}
-        useKeyboardArrows
-      >
+      <Carousel autoFocus={true} showThumbs={false} showStatus={false} useKeyboardArrows>
         {renderImgs(temp)}
       </Carousel>
     </>
