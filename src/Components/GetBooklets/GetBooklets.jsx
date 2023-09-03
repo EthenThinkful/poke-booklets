451d0abaee62357e322875382442b08369477f1f
Ethen Roth <Ethenfm@gmail.com>
2023-09-03 13:05:06 -0600

    added onKey events for navigating dropdown list and submitting get requests. Re-route a person from create booklet page to home page upon submitting their booklet.

--- src/Components/GetBooklets/GetBooklets.jsx
+++ src/Components/GetBooklets/GetBooklets.jsx
@@ -5,7 +5,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Link } from "react-router-dom";
 
-const serverAddress = import.meta.env.VITE_PROD_URL
+const serverAddress = import.meta.env.VITE_PROD_URL;
 // const serverAddress = import.meta.env.VITE_DEV_URL
 
 export default function GetBooklets() {
@@ -43,68 +43,78 @@ export default function GetBooklets() {
     temp.push(
       <div className="lg:flex">
         {chunk.map((item) => (
-      <div className="text-neutral-700 text-sm lg:flex lg:justify-center lg:items-center lg: mx-2">
-        <div>
-          <div className="flex justify-between lg:px-8">
-          {item.userName}'s party
-          {/* <button Link>Edit</button> */}
-          <button
-            onClick={() =>
-              axios
-                .delete(
-                  `${serverAddress}/api/booklet/${item.id}`
-                )
-                .then((res) => {
-                  toast("Booklet deleted successfully!");
-                  // console.log(temp);
-                  setIsDeleted(true);
-                })
-            }
-          >
-            Delete
-          </button>
+          <div className="text-neutral-700 text-sm lg:flex lg:justify-center lg:items-center lg: mx-2">
+            <div>
+              <div className="flex justify-between lg:px-8">
+                {item.userName}'s party
+                {/* <button Link>Edit</button> */}
+                <button
+                  onClick={() => {
+                    const shouldDelete = window.confirm(
+                      "Are you sure you want to delete this booklet?"
+                    );
+                    if (shouldDelete) {
+                      axios
+                        .delete(`${serverAddress}/api/booklet/${item.id}`)
+                        .then((res) => {
+                          toast("Booklet deleted successfully!");
+                          // console.log(temp);
+                          setIsDeleted(true);
+                        });
+                    }
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+              <div className="card__book mt-2">
+                <div className="card__slot">
+                  <DraggablePictureTwo src={item.cardOne} id={item.id} />
+                </div>
+                <div className="card__slot">
+                  <DraggablePictureTwo src={item.cardTwo} id={item.id} />
+                </div>
+                <div className="card__slot">
+                  <DraggablePictureTwo src={item.cardThree} id={item.id} />
+                </div>
+                <div className="card__slot">
+                  <DraggablePictureTwo src={item.cardFour} id={item.id} />
+                </div>
+                <div className="card__slot">
+                  <DraggablePictureTwo src={item.cardFive} id={item.id} />
+                </div>
+                <div className="card__slot">
+                  <DraggablePictureTwo src={item.cardSix} id={item.id} />
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="card__book mt-2">
-            <div className="card__slot">
-              <DraggablePictureTwo src={item.cardOne} id={item.id} />
-            </div>
-            <div className="card__slot">
-              <DraggablePictureTwo src={item.cardTwo} id={item.id} />
-            </div>
-            <div className="card__slot">
-              <DraggablePictureTwo src={item.cardThree} id={item.id} />
-            </div>
-            <div className="card__slot">
-              <DraggablePictureTwo src={item.cardFour} id={item.id} />
-            </div>
-            <div className="card__slot">
-              <DraggablePictureTwo src={item.cardFive} id={item.id} />
-            </div>
-            <div className="card__slot">
-              <DraggablePictureTwo src={item.cardSix} id={item.id} />
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
-      ))}
-    </div>
     );
   }
 
   return (
     <div className="iphone__screen">
       <div className="flex justify-between items-center">
-      <div className="text-neutral-700 flex items-center">Poke Booklets</div>
-      <div className="text-neutral-700 flex justify-center items-center bg-red-200 rounded-xl text-xs h-10 lg:w-1/12">
-      <Link to={'/create'}>Create Yours!</Link>
-      </div>
+        <div className="text-neutral-700 flex items-center">Poke Booklets</div>
+        <div className="text-neutral-700 flex justify-center items-center bg-red-200 rounded-xl text-xs h-10 lg:w-1/12">
+          <Link to={"/create"}>Create Yours!</Link>
+        </div>
       </div>
       {/* <div className="text-neutral-700 text-xs rounded-lg flex text-left items-center w-6/12 mb-12 lg:items-center lg:justify-center lg:text-center mx-auto"></div> */}
-      <div className="text-neutral-700 text-xs rounded-lg flex mt-4 mb-8 lg:mb-12 lg:mt-0 w-10/12 lg:w-6/12 lg:items-center lg:justify-center text-left lg:mx-0">Poke Booklets is for people who want to interact with others over their Pokemon card collectibles through their digital booklets.</div>
-      {temp.length > 0 ? temp.map((item) => {
-        // console.log(item);
-        return item;
-      }): <div className="loader"></div>}
+      <div className="text-neutral-700 text-xs rounded-lg flex mt-4 mb-8 lg:mb-12 lg:mt-0 w-10/12 lg:w-6/12 lg:items-center lg:justify-center text-left lg:mx-0">
+        Poke Booklets is for people who want to interact with others over their
+        Pokemon card collectibles through their digital booklets.
+      </div>
+      {temp.length > 0 ? (
+        temp.map((item) => {
+          // console.log(item);
+          return item;
+        })
+      ) : (
+        <div className="loader"></div>
+      )}
     </div>
   );
 }
--- src/Components/SearchCard/SearchCard.jsx
+++ src/Components/SearchCard/SearchCard.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 import pokeNames from "../../assets/PokeJSON/pokeNames.json";
 import pokemon from "pokemontcgsdk";
 import SearchForm from '../SearchForm/SearchForm';
@@ -8,13 +8,67 @@ import UserForm from "../UserForm/UserForm";
 pokemon.configure({ apiKey: import.meta.env.VITE_TCG_API });
 
 export default function SearchCard({setBook, book, setUserName, userName, toast, setCard, card, newArray, poke, setPoke}) {
+  const [focusedIndex, setFocusedIndex] = useState(-1);
+  const dropdownRef = useRef(null);
 
-    function getCard() {
-        pokemon.card.where({ q: `name:${poke}` }).then((result) => {
+    function getCard(test) {
+        pokemon.card.where({ q: `name:${test}` }).then((result) => {
           console.log("HERE MIGHT BE THE ERROR")
           setCard([result.data]);
         });
       }
+
+      useEffect(() => {
+        const handleKeyDown = (event) => {
+          if (event.key === "ArrowDown") {
+            event.preventDefault();
+            setFocusedIndex((prevIndex) => {
+              const nextIndex = prevIndex + 1;
+              return nextIndex;
+            });
+          } else if (event.key === "ArrowUp") {
+            event.preventDefault();
+            setFocusedIndex((prevIndex) => {
+              const nextIndex = prevIndex - 1;
+              return nextIndex;
+            });
+          } else if (event.key === "Enter") {
+            event.preventDefault();
+            if (currPokemons[focusedIndex]) {
+              let testVar = currPokemons[focusedIndex].props.children.toLocaleLowerCase();
+              setPoke(currPokemons[focusedIndex].props.children.toLocaleLowerCase());
+              getCard(testVar);
+            }
+            setFocusedIndex(-1);
+          }
+        };
+    
+        document.addEventListener("keydown", handleKeyDown);
+        return () => {
+          document.removeEventListener("keydown", handleKeyDown);
+        };
+      }, [focusedIndex, setPoke]);
+
+      let currPokemons = pokeNames
+      .filter((item) => {
+        const searchTerm = poke.toLocaleLowerCase();
+        const fullName = item.toLocaleLowerCase();
+        return (
+          searchTerm &&
+          fullName.startsWith(searchTerm) &&
+          fullName !== searchTerm
+        );
+      })
+      .slice(0, 6)
+      .map((item, index) => (
+        <div
+          onClick={() => setPoke(item.toLocaleLowerCase())}
+          key={item}
+          className={`cursor-pointer p-2 ${index === focusedIndex ? 'bg-gray-200' : ''}`}
+        >
+          {item}
+        </div>
+      ))
 
   return (
     <>
@@ -23,29 +77,9 @@ export default function SearchCard({setBook, book, setUserName, userName, toast,
     <SearchForm setPoke={setPoke} poke={poke} setCard={setCard} getCard={getCard}/>
         </div>
         <div className="drop__down text-xs mb-2 mt-3 text-zinc-700">
-          {pokeNames
-            .filter((item) => {
-              const searchTerm = poke.toLocaleLowerCase();
-              const fullName = item.toLocaleLowerCase();
-              return (
-                searchTerm &&
-                fullName.startsWith(searchTerm) &&
-                fullName !== searchTerm
-              );
-            })
-            .slice(0, 6)
-            .map((item) => (
-              <div
-                onClick={() => setPoke(item.toLocaleLowerCase())}
-                key={item}
-                className="cursor-pointer p-2"
-              >
-                {item}
-              </div>
-            ))}
+          {currPokemons}
         </div>
         <RenderCarousel carouselImg={newArray} setBook={setBook} />
-        {/* <UserForm setBook={setBook} book={book} setUserName={setUserName} userName={userName} toast={toast} setCard={setCard} setPoke={setPoke}/> */}
     </>
   )
 }
