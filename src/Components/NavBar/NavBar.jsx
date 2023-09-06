d639499fba4d1b2ea385210bd282ea43a0d6f826
yutaokkots <yuta.okkotsu@gmail.com>
2023-09-05 22:50:27 -0600

    updated NavBar.jsx to include new logo draft, SearchForm.jsx to rearrange button and clear up styling, and RenderCarousel.jsx to clean up styling

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import pokeLogo from "../../assets/pokebooklet.svg"
+
 
 export default function NavBar() {
   return (
     <div className="h-[8vh] lg:h-[10vh] w-full bg-red-200 flex items-center justify-between px-6 lg:px-10 text-xs lg:text-sm caret-transparent">
-      <Link to={'/'}><div>Logo</div></Link>
+      <Link to={'/'}><div><img src={pokeLogo}></img></div></Link>
       <div className="flex flex-row">
         <Link to={'/home'}><div className="ml-4 lg:ml-8">Home</div></Link>
         <Link to={'/create'}><div className="ml-4 lg:ml-8">Create</div></Link>
