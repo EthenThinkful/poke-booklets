3f6c5651eda8d92a9ed4ae43fb6977d9049033b4
EthenThinkful <ethenfm@gmail.com>
2023-10-17 00:17:22 -0600

    user can update their display name

--- src/Components/UserProfileComponents/UserDetails/UserDetails.jsx
+++ src/Components/UserProfileComponents/UserDetails/UserDetails.jsx
@@ -1,10 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { UilEdit } from "@iconscout/react-unicons";
+import axios from "axios";
 
 function UserDetails({ serverAddress, totalCards }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [trainerName, setTrainerName] = useState(null);
+
+  useEffect(() => {
+    axios.get(`${serverAddress}/api/userss/${localStorage.ID}`).then((res) => {
+      setTrainerName(res.data.nickName);
+    });
+  }, []);
+
+  const handleEditClick = () => {
+    setIsEditing(!isEditing);
+  };
+
+  const handleSaveClick = () => {
+    setIsEditing(false);
+    const data = { id: localStorage.ID, nickName: trainerName };
+    axios
+      .put(`${serverAddress}/api/userss`, data)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const handleInputChange = (event) => {
+    setTrainerName(event.target.value);
+  };
+
   return (
     <div className="flex flex-col justify-center p-2 ml-2 text-[9px] text-white text-black bg-orange-300 rounded-lg sm:justify-evenly  sm:text-[16px] sm:p-4">
-      <span>Trainer ID: 1</span>
-      <span>Trainer Name: Ethan</span>
+      <div className="flex justify-between">
+        <span>Trainer ID: 1</span>
+        <UilEdit onClick={handleEditClick} />
+      </div>
+      {isEditing ? (
+        <form>
+          <label>
+            Trainer Name:
+            <input
+              type="text"
+              value={trainerName}
+              onChange={handleInputChange}
+              className="rounded-md text-black pl-2"
+            />
+          </label>
+          <button type="button" className="ml-2" onClick={handleSaveClick}>
+            Save
+          </button>
+        </form>
+      ) : (
+        <span>Trainer Name: {trainerName}</span>
+      )}
       <span>Total Cards: {totalCards}</span>
       <span>Notable Pokemon: Crabominable</span>
     </div>
