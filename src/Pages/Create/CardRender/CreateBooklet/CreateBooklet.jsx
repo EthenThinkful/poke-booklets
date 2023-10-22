ef8a00c711a283988ffa30117fe05ada8b4a4e92
EthenThinkful <ethenfm@gmail.com>
2023-10-22 01:12:48 -0600

    carousel profile booklet

--- src/Pages/Create/CardRender/CreateBooklet/CreateBooklet.jsx
+++ src/Pages/Create/CardRender/CreateBooklet/CreateBooklet.jsx
@@ -4,6 +4,7 @@ import { useDrag, useDrop } from "react-dnd";
 import axios from "axios";
 import WebcamCapture from "./WebcamCampture";
 import { UilCheckCircle } from "@iconscout/react-unicons";
+import BookletCarousel from "../../../Home/GetBooklets/BookletCarousel";
 
 export default function CreateBooklet({
   setBook,
@@ -22,18 +23,10 @@ export default function CreateBooklet({
   cardInfo,
   setCardInfo,
 }) {
-  const [{ isOverTrash }, dropTrash] = useDrop(() => ({
-    accept: "image",
-    drop: (item) => handleRemoveItem(item),
-    collect: (monitor) => ({
-      isOver: !!monitor.isOver(),
-    }),
-  }));
-
-  function handleRemoveItem(item) {
+  function handleRemoveItem(itemId) {
     // console.log(item.id.current);
     axios
-      .delete(`${serverAddress}/api/cards/${item.id.current}`)
+      .delete(`${serverAddress}/api/cards/${itemId}`)
       .then((res) => {
         setReload(!reload);
         // console.log(res);
@@ -48,36 +41,40 @@ export default function CreateBooklet({
     });
   }, [reload]);
 
+  // method to delete a card from your booklet (changing delete functionality from trash can to this)
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+  const [deleteCard, setDeleteCard] = useState(null);
+  
+  function handleDeleteClick (itemId) {
+    setShowDeleteConfirmation(true);
+    setDeleteCard(itemId);
+  };
+
+  const handleConfirmDelete = () => {
+    handleRemoveItem(deleteCard);
+    setShowDeleteConfirmation(false);
+  };
+
+  const handleCancelDelete = () => {
+    setShowDeleteConfirmation(false);
+    console.log("cancel delete");
+  };
+
+  console.log(showDeleteConfirmation)
+  // end method to delete a card from your booklet (changing delete functionality from trash can to this)
+
   return (
     <>
       <div className="lg:flex lg:justify-center lg:items-center lg:mb-6 max-w-[340px] mx-auto card__book__width lg:w-full lg:max-w-full lg:mx-4">
-        <div className="card__book mt-2 w-full max-w-full mx-auto lg:max-w-[520px]">
-          {[...Array(6)].map((_, index) => (
-            <div className="card__slot" key={index}>
-              {cardInfo.length > index && (
-                <>
-                  <div className="group relative">
-                    <DraggablePictureTwo
-                      src={cardInfo[index].pokemonCard}
-                      id={cardInfo[index].id}
-                    />
-                    {cardInfo[index].verified === true ? (
-                      <div className="bg-blue-500 absolute ml-[5rem] top-[7rem] lg:ml-36 lg:top-[12.5rem] lg:left-[-5px] rounded-xl transform scale-100 group-hover:scale-150 group-hover:translate-x-[1rem] lg:group-hover:translate-x-[2rem] group-hover:translate-y-6 lg:group-hover:translate-y-10 transition-transform duration-300 text-white">
-                        <UilCheckCircle />
-                      </div>
-                    ) : null}
-                  </div>
-                </>
-              )}
-            </div>
-          ))}
+        <BookletCarousel item={cardInfo} showDeleteConfirmation={showDeleteConfirmation} setShowDeleteConfirmation={setShowDeleteConfirmation}
+        handleDeleteClick={handleDeleteClick} handleConfirmDelete={handleConfirmDelete} handleCancelDelete={handleCancelDelete}/>
+        {showDeleteConfirmation && (
+        <div className="w-100 h-100 bg-slate-700 animate-pulse">
+          <p>Delete card?</p>
+          <button onClick={handleConfirmDelete}>Yes</button>
+          <button onClick={handleCancelDelete}>No</button>
         </div>
-        <div
-          className="trash__can bg-slate-600 w-[90px] h-[125px] sm:w-[150px] sm:h-[209px] mx-auto mb-4 m-0 rounded-lg flex text-center justify-center p-10 pt-6 pb-6 text-xs lg:text-sm mt-2 lg:p-12 lg:pt-10 lg:m-10 caret-transparent"
-          ref={dropTrash}
-        >
-          drag & drop cards here to delete
-        </div>
+      )}
         <WebcamCapture
           serverAddress={serverAddress}
           reload={reload}
